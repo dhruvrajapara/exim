82796ff2e22d1d3e76b5fc8670282bc0e83b856c
dhruvrajapara <dhruvrajapara2805@gmail.com>
2026-07-27 18:42:02 +0530

    bug fix

--- frontend/src/pages/admin/AdminLogin.jsx
+++ frontend/src/pages/admin/AdminLogin.jsx
@@ -91,7 +91,7 @@ export default function AdminLogin() {
                   autoComplete="email"
                   required
                   className="focus:ring-primary focus:border-primary block w-full pl-10 sm:text-sm border-gray-300 rounded-md py-3 border px-4 outline-none transition-colors"
-                  placeholder="admin@biteexport.com"
+                  placeholder="admin@example.com"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                 />
